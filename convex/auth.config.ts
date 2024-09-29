3571213d1610adf3754666d42dd9ffc63ec3287b
Mladen Antic <mladen.antic98@gmail.com>
2024-09-29 15:04:10 +0200

    Setting up clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://current-eel-64.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
